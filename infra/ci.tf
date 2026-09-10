6a4774af8de70ea80d477b58457499ad3d8d6e4f
Ylli Ahmeti <yahmeti@cord.edu>
2026-09-10 21:57:10 +0200

    Grant CI user glue GetTableVersions permissions

--- infra/ci.tf
+++ infra/ci.tf
@@ -30,7 +30,8 @@ resource "aws_iam_user_policy" "ci" {
         Effect = "Allow"
         Action = [
           "glue:GetDatabase", "glue:GetDatabases", "glue:CreateDatabase",
-          "glue:GetTable", "glue:GetTables", "glue:CreateTable", "glue:UpdateTable", "glue:DeleteTable",
+          "glue:GetTable", "glue:GetTables", "glue:GetTableVersion", "glue:GetTableVersions",
+          "glue:CreateTable", "glue:UpdateTable", "glue:DeleteTable",
           "glue:GetPartition", "glue:GetPartitions", "glue:BatchCreatePartition", "glue:CreatePartition", "glue:DeletePartition"
         ]
         Resource = "*"
